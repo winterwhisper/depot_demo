06fae49f1b664f6f7bbab068a02c0e61e0ba6036
winterwhisper <winterwhisper@winterwhisper-OptiPlex-990.(none)>
2013-03-11 18:36:22 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '69c5cda351f2b6cb043920e10efdf60f322e62d774722c02e8e29672e38dfa50f67c41b6d75d18d6687768a3020a2fda9a617fca2fa4b088fae0299a1e7a28e8'
