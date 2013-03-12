7ffc4889137c9d1a560f7598970d73b85da83a63
Vincent Tsao <caojunvincent@gmail.com>
2013-03-12 19:33:53 +0800

    clean route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Depot::Application.routes.draw do
-  resources :line_items
-
 
   resources :carts
-
-
-  get "store/index"
-
   resources :products
+  resources :line_items
 
 
   # The priority is based upon order of creation:
