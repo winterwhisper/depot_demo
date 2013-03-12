9464943445154ae238c51e9f35ffe015cbd2ea95
winterwhisper <winterwhisper.dev@gmail.com>
2013-03-12 11:28:21 +0800

    taskC`s playtime

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -15,6 +15,10 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select '#columns #side a', minimum: 4
+    assert_select '#main tr', 3
+    assert_select 'dt', 'Programming Ruby 1.9'
+    assert_select '#main td.list_actions a', 9
   end
 
   test "should get new" do
