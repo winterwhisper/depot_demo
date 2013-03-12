6531deb717cb3d54a6fd22fdb8109a9d253371bd
winterwhisper <winterwhisper.dev@gmail.com>
2013-03-12 13:39:39 +0800

    taskD

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   attr_accessible :description, :image_url, :price, :title
 
   validates :title, :description, :image_url, presence: true
@@ -9,4 +13,14 @@ class Product < ActiveRecord::Base
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
 
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
+  end
 end
