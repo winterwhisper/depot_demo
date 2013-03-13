d93a571d153bb6e83f9e0da5cae5f86968a3b505
winterwhisper <winterwhisper.dev@gmail.com>
2013-03-13 16:11:38 +0800

    taskI iteration I1 adding users

--- test/functional/orders_controller_test.rb
+++ test/functional/orders_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class OrdersControllerTest < ActionController::TestCase
   setup do
     @order = orders(:one)
+    @payment_types = ([] << payment_types(:one)).map(&:payment_type)
   end
 
   test "should get index" do
@@ -29,12 +30,10 @@ class OrdersControllerTest < ActionController::TestCase
   end
 
   test "should create order" do
-    @payment_types = ([] << payment_types(:one)).map(&:payment_type)
     assert_difference('Order.count') do
       post :create, order: { address: @order.address, email: @order.email, name: @order.name, pay_type: @order.pay_type }
     end
 
-    # assert_redirected_to order_path(assigns(:order))
     assert_redirected_to store_path
   end
 
