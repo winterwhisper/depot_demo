8c3f851e23055452d22a4a1f50795f9498a4f530
winterwhisper <winterwhisper.dev@gmail.com>
2013-03-15 11:24:56 +0800

    add password reset fields to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
 class User < ActiveRecord::Base
   # attr_accessor :auth_token
-  attr_accessible :name, :password_digest, :password, :password_confirmation, :auth_token, :email
+  attr_accessible :name, :password_digest, :password, :password_confirmation, :auth_token, :email, 
+                  :password_reset_token, :password_reset_sent_at
   has_secure_password
   validates :name, presence: true, uniqueness: true
   after_destroy :ensure_an_admin_remains
-  before_create :generate_token#(:auth_token)
+  before_create {|controller| controller.generate_token(:auth_token) }
 
   private
   
@@ -14,11 +15,17 @@ class User < ActiveRecord::Base
     end
   end
 
-  def generate_token#(column)
+  def generate_token(column)
     begin
-      #self[column] = SecureRandom.urlsafe_base64
-      self.auth_token = SecureRandom.urlsafe_base64
-    end while User.exists?("auth_token" => self.auth_token)#User.exists?(column => self[column])
+      self[column] = SecureRandom.urlsafe_base64
+    end while User.exists?(column => self[column])
+  end
+
+  def send_password_reset
+    generate_token(:password_reset_token)
+    self.password_reset_sent_at = Time.zone.now
+    save!
+    UserMailer.password_reset(self).deliver
   end
 end
 
@@ -26,12 +33,14 @@ end
 #
 # Table name: users
 #
-#  id              :integer          not null, primary key
-#  name            :string(255)
-#  password_digest :string(255)
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
-#  auth_token      :string(255)
-#  email           :string(255)
+#  id                     :integer          not null, primary key
+#  name                   :string(255)
+#  password_digest        :string(255)
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  auth_token             :string(255)
+#  email                  :string(255)
+#  password_reset_token   :string(255)
+#  password_reset_sent_at :datetime
 #
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130315031018) do
+ActiveRecord::Schema.define(:version => 20130315032432) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -53,10 +53,12 @@ ActiveRecord::Schema.define(:version => 20130315031018) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "password_digest"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",             :null => false
+    t.datetime "updated_at",             :null => false
     t.string   "auth_token"
     t.string   "email"
+    t.string   "password_reset_token"
+    t.datetime "password_reset_sent_at"
   end
 
 end
