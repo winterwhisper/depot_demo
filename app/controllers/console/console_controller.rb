3a94fa6a4b2cd3892defa3757bb12d1565aa0ac0
winterwhisper <winterwhisper.dev@gmail.com>
2013-03-18 15:23:21 +0800

    add console controller as the console controllers` base

--- app/controllers/console/console_controller.rb
+++ app/controllers/console/console_controller.rb
@@ -1,3 +1,15 @@
-class Console::ConsoleController < ActionController::Base
+class Console::ConsoleController < ApplicationController
   layout 'console'
+
+  protected
+
+  def authorize(role)
+    if current_user.nil?
+      redirect_to console_login_url, notice: "Please log in!"
+    else
+      if current_user.role < Settings.roles.fetch(role.capitalize)
+        redirect_to console_url, notice: "You can`t access this page."
+      end
+    end
+  end
 end
